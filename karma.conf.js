5b52955aa27f7b3f9248ef91380724bda61cb470
thatlittlegit <wapidstyle@live.ca>
2018-03-25 18:48:28 -0400

    ci: Add code coverage with Codecov

--- karma.conf.js
+++ karma.conf.js
@@ -2,11 +2,19 @@ module.exports = (config) => {
 	config.set({
 		frameworks: ['chai', 'mocha'],
 		files: ['*.js'],
-		reporters: ['spec'],
+		reporters: ['spec', 'coverage'],
 		port: 9876,
 		colors: true,
 		browsers: ['Firefox'],
 		singleRun: true,
 		concurrency: Infinity,
+
+		preprocessors: {
+			'index.js': ['coverage']
+		},
+		coverageReporter: {
+			type: 'lcov',
+			dir: 'coverage/'
+		}
 	});
 };
