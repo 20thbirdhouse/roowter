968c389bc2c342ce90a92ae78164af50921653cf
thatlittlegit <personal@thatlittlegit.tk>
2018-06-09 20:44:06 -0400

    refactor(typescript): Add TypeScript typings for the event handler system

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,10 @@
+interface Event {
+	regex: RegExp;
+	callback: () => any;
+}
+
+declare var _events: Event[];
 declare function setRoute(route: string): string;
+declare function onRouteSwitch(regex: RegExp | string, callback: () => any, once: boolean): void;
+declare function onRouteSwitch(regex: RegExp | string, callback: () => any): void;
 declare function initializeRouteButtons(elements: HTMLElement[] | void): void;
