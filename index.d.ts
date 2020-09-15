90942a092df5e9e80971d051fc85dcb2f68a56b3
thatlittlegit <personal@thatlittlegit.tk>
2020-09-15 17:06:46 -0400

    fix(typescript): Remove Event and _events from d.ts
    BREAKING CHANGE: Event and _events are no longer defined in TypeScript.
    There is no replacement.
    
    These were private variables/interfaces, and there's no reason why anyone
    should be using them.

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,3 @@
-interface Event {
-	regex: RegExp;
-	callback: () => any;
-}
-
-declare var _events: Event[];
 declare function setRoute(route: string): string;
 declare function onRouteSwitch(regex: RegExp | string, callback: () => any, once: boolean): void;
 declare function onRouteSwitch(regex: RegExp | string, callback: () => any): void;
